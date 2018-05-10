fixing normalization problem

solver.py
@@ -68,12 +68,23 @@ class Solver(object):
     self.GPU = config.GPU
     self.AU = config.AU
     self.SHOW_MODEL = config.SHOW_MODEL
+    self.TEST_TXT = config.TEST_TXT
+    self.TEST_PTH = config.TEST_PTH
+    self._255 = config._255
 
     self.DONE=False
+    self.string_='00'
+    self.TRAINED_FILE = os.path.join(self.model_save_path, 'TRAINED')
+    if self.TEST_TXT or self.TEST_PTH:
+      os.system('touch {}'.format(self.TRAINED_FILE))
+
     if self.pretrained_model and not self.SHOW_MODEL:
-      if os.path.isfile(os.path.join(self.model_save_path, '{}_00.txt'.format(self.pretrained_model))):
+      if os.path.isfile(self.TRAINED_FILE):
         print("!!!Model already trained")
+        self.TEST_TXT = True
         self.DONE=True
+
+    if self.TEST_TXT: return
 
     # Build tensorboard if use
     self.build_model()
@@ -93,10 +104,14 @@ class Solver(object):
     from torchviz import make_dot, make_dot_from_trace
     from utils import pdf2png
     input_ = self.to_var(torch.randn(1,3,224,224))
+    start_time = time.time()
     if self.OF_option!='None':
       y = self.C(input_, input_)
     else:
       y = self.C(input_)
+    elapsed = time.time() - start_time
+    elapsed = str(datetime.timedelta(seconds=elapsed)) 
+    print("Forward time: "+elapsed)
     g=make_dot(y, params=dict(self.C.named_parameters()))
     filename='misc/VGG16-OF_{}'.format(self.OF_option)
     g.filename=filename
@@ -107,12 +122,7 @@ class Solver(object):
 
   #=======================================================================================#
   #=======================================================================================#
-  def build_model(self):
-    # Define a generator and a discriminator
-    if self.DONE: return
-    from models.vgg16 import Classifier
-    self.C = Classifier(pretrained=self.finetuning, OF_option=self.OF_option, model_save_path=self.model_save_path) 
-
+  def get_trainable_params(self):
     trainable_params = self.C.parameters()
     name_params = sorted([name for name,param in nn.Module.named_parameters(self.C)], reverse=True)
 
@@ -122,9 +132,20 @@ class Solver(object):
 
     if self.HYDRA: 
       trainable_params = self.C.model.classifier.parameters()
-      name_params = sorted([name for name,param in nn.Module.named_parameters(self.C) if 'features' not in name], reverse=True)
+      name_params = sorted([name for name,param in nn.Module.named_parameters(self.C) if 'features' not in name], reverse=True)    
 
-    # ipdb.set_trace()
+    return trainable_params, name_params
+
+  #=======================================================================================#
+  #=======================================================================================#
+  def build_model(self):
+    # Define a generator and a discriminator
+    if self.TEST_TXT: return
+    from models.vgg16 import Classifier
+    self.C = Classifier(pretrained=self.finetuning, OF_option=self.OF_option, model_save_path=self.model_save_path) 
+
+    trainable_params, name_params = self.get_trainable_params()
+
     print("==============\nTrainable layers:\n{}\n==============".format(str(name_params)))
     # Optimizer
     # self.optimizer = torch.optim.Adam(self.C.parameters(), self.lr, [self.beta1, self.beta2])
@@ -144,19 +165,27 @@ class Solver(object):
     num_params = 0
     for p in model.parameters():
       num_params += p.numel()
+
+    num_learn_params = 0
+    learnable_params, _ = self.get_trainable_params()
+    for p in learnable_params:
+      num_learn_params += p.numel()
+
+    # ipdb.set_trace()      
     if self.SHOW_MODEL: 
       print(name)
       print(model)
     print("The number of parameters (OF: {}): {}".format(self.OF_option, num_params))
+    print("The number of learnable parameters (OF: {}): {}".format(self.OF_option, num_learn_params))
 
   #=======================================================================================#
   #=======================================================================================#
   def load_pretrained_model(self):
-    if self.DONE: return
+    # if self.DONE: return
     model = os.path.join(
       self.model_save_path, '{}.pth'.format(self.pretrained_model))
     self.C.load_state_dict(torch.load(model))
-    print('loaded trained model: {}!'.format(model))
+    print(' [!!] loaded trained model: {}!'.format(model))
 
   #=======================================================================================#
   #=======================================================================================#
@@ -212,9 +241,6 @@ class Solver(object):
     
     # Start with trained model if exists
     if self.pretrained_model:
-      if os.path.isfile(os.path.join(self.model_save_path, '{}_00.txt'.format(self.pretrained_model))):
-        print("!!!Model already trained")
-        return
       start = int(self.pretrained_model.split('_')[0])
       # Decay learning rate
       for i in range(start):
@@ -258,7 +284,7 @@ class Solver(object):
         print("--> RGB and OF # lines: %d - %d"%(len(self.rgb_loader), len(of_loader)))
 
       for i, (rgb_img, rgb_label, rgb_files) in tqdm.tqdm(enumerate(self.rgb_loader), \
-          total=len(self.rgb_loader), desc='Epoch: %d/%d | %s'%(e,self.num_epochs, Log)):
+          total=len(self.rgb_loader), ncols=10, desc='Epoch: %d/%d | %s'%(e,self.num_epochs, Log)):
         # ipdb.set_trace()
         rgb_img = self.to_var(rgb_img)
         rgb_label = self.to_var(rgb_label)
@@ -314,7 +340,7 @@ class Solver(object):
       # if loss_val<loss_val_prev:
       if f1_val>f1_val_prev:
         torch.save(self.C.state_dict(), os.path.join(self.model_save_path, '{}_{}.pth'.format(E, i+1)))   
-        os.system('rm {}'.format(os.path.join(self.model_save_path, '{}_{}.pth'.format(E-1, i+1))))
+        os.system('rm -vf {}'.format(os.path.join(self.model_save_path, '{}_{}.pth'.format(str(int(E)-1).zfill(2), i+1))))
         # loss_val_prev = loss_val
         print("! Saving model")
         f1_val_prev = f1_val
@@ -324,7 +350,12 @@ class Solver(object):
         non_decreasing+=1
         if non_decreasing == self.stop_training:
           print("During {} epochs LOSS VAL was not decreasing.".format(self.stop_training))
-          return            
+          os.system('touch {}'.format(self.TRAINED_FILE))
+          return          
+        # else:
+        #   lr = (self.lr / 10)
+        #   self.update_lr(lr)          
+        #   print ('Decay learning rate to: {}.'.format(lr))  
 
       # Decay learning rate
       if (e+1) > (self.num_epochs - self.num_epochs_decay):
@@ -390,33 +421,34 @@ class Solver(object):
     D_path = os.path.join(self.model_save_path, '{}.pth'.format(last_name))
     txt_path = os.path.join(self.model_save_path, '{}_{}.txt'.format(last_name,'{}'))
     self.pkl_data = os.path.join(self.model_save_path, '{}_{}.pkl'.format(last_name, '{}'))
-    print(" [!!] {} model loaded...".format(D_path))
+    # print(" [!!] {} model loaded...".format(D_path))
     # self.G.load_state_dict(torch.load(G_path))
     if not self.DONE: 
       self.C.load_state_dict(torch.load(D_path))
       self.C.eval()
     # ipdb.set_trace()
     data_loader_val = get_loader(self.metadata_path, self.image_size,
-                   self.image_size, self.batch_size, 'val')
+                   self.image_size, self.batch_size, 'val', _255=self._255, imagenet=self.finetuning=='imagenet')
     data_loader_test = get_loader(self.metadata_path, self.image_size,
-                   self.image_size, self.batch_size, 'test')
+                   self.image_size, self.batch_size, 'test', _255=self._255, imagenet=self.finetuning=='imagenet')
 
     if self.OF:
       of_loader_val = get_loader(self.metadata_path, self.image_size,
-                     self.image_size, self.batch_size, 'val', OF=True)        
+                     self.image_size, self.batch_size, 'val', OF=True, _255=self._255, imagenet=self.finetuning=='imagenet')        
       of_loader_test = get_loader(self.metadata_path, self.image_size,
-                   self.image_size, self.batch_size, 'test', OF=True)   
+                   self.image_size, self.batch_size, 'test', OF=True, _255=self._255, imagenet=self.finetuning=='imagenet')   
 
     if not hasattr(self, 'output_txt'):
       # ipdb.set_trace()
       self.output_txt = txt_path
+      # ipdb.set_trace()
       try:
-        self.output_txt = sorted(glob.glob(self.output_txt.format('*')))[-1]
-        number_file = len(glob.glob(self.output_txt))
+        txt_file = sorted(glob.glob(self.output_txt.format('*')))
+        number_file = len(txt_file)
       except:
         number_file = 0
-      self.output_txt = self.output_txt.format(str(number_file).zfill(2)) 
-    
+      self.output_txt = self.output_txt.format(str(number_file).zfill(len(self.string_))) 
+    print("Output directly to: {}".format(self.output_txt))
     self.f=open(self.output_txt, 'a')  
     self.thresh = np.linspace(0.01,0.99,200).astype(np.float32)
     # ipdb.set_trace()

utils.py
@@ -7,6 +7,7 @@ import ipdb
 import math
 import xlsxwriter
 import openpyxl
+import tqdm
 from openpyxl.styles import Color, PatternFill, colors, Font
 
 def f1_score_max(gt, pred, thresh):
@@ -33,8 +34,7 @@ def f1_score_max(gt, pred, thresh):
 
   return F1, F1_MAX, F1_THRESH
 
-def f1_score(gt, pred, F1_Thresh=0.5, median=False):
-  import pandas
+def f1_score(gt, pred, F1_Thresh=0.5, files=None, median=False):  
   from sklearn.metrics import precision_score, recall_score
   from sklearn.metrics import f1_score as f1s
   if type(gt)==list: gt = np.array(gt)
@@ -46,19 +46,51 @@ def f1_score(gt, pred, F1_Thresh=0.5, median=False):
   F1_MAX=F1
 
   if median:
-    # ipdb.set_trace()
-    output_median3 = np.array(pandas.Series(output).rolling(window=3, center=True).median().bfill().ffill())
+    output_median3, output_median5, output_median7 = get_median(output, files)
     F1_median3 = f1s(gt, output_median3)
-
-    output_median5 = np.array(pandas.Series(output).rolling(window=5, center=True).median().bfill().ffill())
     F1_median5 = f1s(gt, output_median5)
-
-    output_median7 = np.array(pandas.Series(output).rolling(window=7, center=True).median().bfill().ffill())
     F1_median7 = f1s(gt, output_median7)
 
     return [F1], F1_MAX, F1_Thresh, F1_median3, F1_median5, F1_median7
   else:
     return [F1], F1_MAX, F1_Thresh 
+
+def get_median(pred, files, mode='video'):
+  import pandas
+  if mode=='video':
+    video_files, video_hist = get_unique_files(files)
+    output_median = []
+    output_median3 = []
+    output_median5 = []
+    output_median7 = []
+    count = 0
+    video = ''
+    for i in range(len(video_hist)): 
+      # assert video!=video_files[count]
+      if video==video_files[count]: ipdb.set_trace()
+      video = video_files[count]
+      range_frames = video_hist[video]
+      output_median = pred[count:count+range_frames]
+      count += range_frames
+
+      output_median3.extend(np.array(pandas.Series(output_median).rolling(window=3, center=True).median().bfill().ffill()))
+      output_median5.extend(np.array(pandas.Series(output_median).rolling(window=5, center=True).median().bfill().ffill()))
+      output_median7.extend(np.array(pandas.Series(output_median).rolling(window=7, center=True).median().bfill().ffill()))
+    # ipdb.set_trace()
+  elif mode=='all':
+    output_median3 = np.array(pandas.Series(pred).rolling(window=3, center=True).median().bfill().ffill())
+    output_median5 = np.array(pandas.Series(pred).rolling(window=5, center=True).median().bfill().ffill())
+    output_median7 = np.array(pandas.Series(pred).rolling(window=7, center=True).median().bfill().ffill())
+
+  return output_median3, output_median5, output_median7
+
+def get_unique_files(files):
+  video_files = [os.path.dirname(line) for line in files]
+  hist_video = {}
+  for file_ in video_files: 
+    if file_ not in hist_video.keys(): hist_video[file_]=1
+    else: hist_video[file_]+=1
+  return video_files, hist_video
 
 def whereAU(au):
   return np.where(np.array(cfg.AUs)==au)[0][0]
@@ -160,8 +192,7 @@ def F1_TEST(config, data_loader, mode = 'TEST', thresh = 0.5, OF= None, verbose=
   import torch.nn.functional as F
   PREDICTION = []
   GROUNDTRUTH = []
-  total_idx=int(len(data_loader)/config.batch_size)  
-  count = 0
+  FILES = []
   loss = []
   if verbose: 
     print("-> xls results at "+config.xlsfile.replace('.xlsx','_'+mode+'.xlsx'))
@@ -170,12 +201,22 @@ def F1_TEST(config, data_loader, mode = 'TEST', thresh = 0.5, OF= None, verbose=
     # ipdb.set_trace()
 
   if OF is not None: of_loader = iter(OF)
-  if verbose: print('\n================================')
-  for i, (real_x, org_c, files) in enumerate(data_loader):
+  if verbose: 
+    print('\n================================')
+    iterator = tqdm.tqdm(enumerate(data_loader), total=len(data_loader), ncols=10, desc="{} forward".format(mode.upper()))
+  else:
+    iterator = enumerate(data_loader)
 
-    if verbose and os.path.isfile(config.pkl_data.format(mode.lower())): 
-      PREDICTION, GROUNDTRUTH = pickle.load(open(config.pkl_data.format(mode.lower())))
-      break
+  flag_continue = True
+  for i, (real_x, org_c, files) in iterator:
+
+    if verbose and config.TEST_TXT and flag_continue: 
+      try:
+        PREDICTION, GROUNDTRUTH, FILES = pickle.load(open(config.pkl_data.format(mode.lower())))
+        break
+      except:
+        flag_continue=False# PREDICTION, GROUNDTRUTH = pickle.load(open(config.pkl_data.format(mode.lower())))
+      
     # ipdb.set_trace()
     real_x = config.to_var(real_x, volatile=True)
     labels = org_c
@@ -189,7 +230,6 @@ def F1_TEST(config, data_loader, mode = 'TEST', thresh = 0.5, OF= None, verbose=
       out_temp = config.C(real_x)
     
     # output = ((F.sigmoid(out_cls_temp)>=0.5)*1.).data.cpu().numpy()
-    # ipdb.set_trace()
     output = F.sigmoid(out_temp)#[:,1]
     # loss.append(F.cross_entropy(out_temp, config.to_var(org_c).squeeze(1)))
     loss.append(config.LOSS(out_temp, config.to_var(org_c)))
@@ -198,18 +238,15 @@ def F1_TEST(config, data_loader, mode = 'TEST', thresh = 0.5, OF= None, verbose=
     #   print("Predicted:   "+str((output>=0.5)*1))
     #   print("Groundtruth: "+str(org_c))
 
-    count += org_c.shape[0]
-    if verbose:
-      string_ = str(count)+' / '+str(len(data_loader)*config.batch_size)
-      sys.stdout.write("\r%s" % string_)
-      sys.stdout.flush()    
+
     # ipdb.set_trace()
 
     PREDICTION.extend(output.data.cpu().numpy().flatten().tolist())
     GROUNDTRUTH.extend(labels.cpu().numpy().astype(np.uint8).tolist())
+    FILES.extend(['/'.join(file_.split('/')[-3:]) for file_ in files])
 
-  if verbose and not os.path.isfile(config.pkl_data.format(mode.lower())): 
-    pickle.dump([PREDICTION, GROUNDTRUTH], open(config.pkl_data.format(mode.lower()), 'w'))
+  if verbose: 
+    pickle.dump([PREDICTION, GROUNDTRUTH, FILES], open(config.pkl_data.format(mode.lower()), 'w'))
   if verbose: 
     print("")
     print >>config.f, ""
@@ -224,10 +261,10 @@ def F1_TEST(config, data_loader, mode = 'TEST', thresh = 0.5, OF= None, verbose=
   prediction = PREDICTION
   groundtruth = GROUNDTRUTH
   if mode=='TEST':
-    _, F1_real5, F1_Thresh5, F1_median3, F1_median5, F1_median7 = f1_score(groundtruth, prediction, 0.5, median=True)  
-    _, F1_real, F1_Thresh, F1_median3_th, F1_median5_th, F1_median7_th = f1_score(np.array(groundtruth), np.array(prediction), thresh, median=True)
+    _, F1_real5, F1_Thresh5, F1_median3, F1_median5, F1_median7 = f1_score(groundtruth, prediction, 0.5, files=FILES, median=True)  
+    _, F1_real, F1_Thresh, F1_median3_th, F1_median5_th, F1_median7_th = f1_score(np.array(groundtruth), np.array(prediction), thresh, files=FILES, median=True)
   else:
-    _, F1_real, F1_Thresh, F1_median3, F1_median5, F1_median7 = f1_score(np.array(groundtruth), np.array(prediction), thresh, median=True)
+    _, F1_real, F1_Thresh, F1_median3, F1_median5, F1_median7 = f1_score(np.array(groundtruth), np.array(prediction), thresh, files=FILES, median=True)
     F1_real5 = F1_real
     F1_median3_th = F1_median3;F1_median5_th = F1_median5;F1_median7_th = F1_median7
   _, F1_0, F1_Thresh_0 = f1_score(np.array(groundtruth), np.zeros_like(prediction), thresh)